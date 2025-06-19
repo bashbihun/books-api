195b1a8bb12430b42d45d98e07fc8e5d22174daf
bashbihun <bashbihun@gmail.com>
2025-06-19 08:51:42 +0700

    update main.go dan route.go serta test.http

--- main.go
+++ main.go
@@ -4,13 +4,21 @@ import (
 	"books-api/config"
 	"books-api/models"
 	"books-api/routes"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+
 	db := config.ConnectDB()
-	db.AutoMigrate(&models.Book{})
+	db.AutoMigrate(&models.Book{}, &models.User{})
 
 	r := gin.Default()
 	routes.SetUpRoute(r, db)
--- routes/route.go
+++ routes/route.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"books-api/controllers"
+	"books-api/middleware"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -12,6 +13,13 @@ func SetUpRoute(r *gin.Engine, db *gorm.DB) {
 		c.Set("db", db)
 	})
 
-	r.GET("/books", controllers.GetBook)
-	r.POST("/books", controllers.CreateBook)
+	r.POST("/register", controllers.Register)
+	r.POST("/login", controllers.Login)
+
+	protected := r.Group("/")
+	protected.Use(middleware.AuthMiddleware())
+	{
+		r.GET("/books", controllers.GetBook)
+		r.POST("/books", controllers.CreateBook)
+	}
 }
